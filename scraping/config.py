64978d27018d4ceb5a3f4c5805c2bed9e05041df
abdelrahmanrezk <abdelrahmanhamdy33@gmail.com>
2020-12-02 21:32:29 +0200

    scrap webTep all arabic drugs

---
+++ scraping/config.py
@@ -0,0 +1,13 @@
+jumia_url_mobile = "https://www.jumia.com.eg/ar/mobile-phones"
+jumia_url_electronics = "https://www.jumia.com.eg/ar/electronics/"
+jumia_url_computing = "https://www.jumia.com.eg/ar/computing/"
+web_teb = "https://www.webteb.com/drug"
+
+gecko_driver = 'driver/geckodriver'
+headless = False
+user_agent = 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0'
+
+#mongo_url = # provide your mongo url 
+#mongo_user = # provide your mongo user
+#mongo_pass = # provide your mongo password
+
